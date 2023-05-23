951dae7dec0ad85d0cfbba1bcafca066f2bb2bc2
Cunsolo Mateo <cunsolo73@gmail.com>
2023-05-22 23:47:42 -0300

    cambios en el jscript de confirm

--- confirm.js
+++ confirm.js
@@ -78,32 +78,59 @@ const checkbox1 = document.querySelector("#checkbox1");
 const checkbox2 = document.querySelector("#checkbox2");
 const contenido = document.querySelector(".contenido");
 
+//declaro la cantidad de hijos que tiene carritoFin antes de que se aprete en algun checkbox
+let hijosCarritoAntes = carritoFin.children.length;
+
+
 //Iniciamos la pagina con checkbox2 que significa que el coste de envio es 0 y el cliente lo retirar por algun punto de encuentro o sucursal
 checkbox2.checked = true;
 
-//text input que van a tener ambas opciones, en este caso indicar la ciudad
-let ciudad = document.createElement("input");
-ciudad.classList.add("direccionCodeCity");
-ciudad.type = "text";
-ciudad.placeholder = "Ingrese su ciudad...";
-// se agrega un hijo a -> contenido: que tiene los check box 1 y 2 respectivamente, ahora va a tener un hijo mas que es el input de ingresar ciudad.
-contenido.appendChild(ciudad);
 // que sucede a marcar la casilla "checkbox1 -> Llegan entre 2 a 5 dias habiles a tu domicilio"
 checkbox1.addEventListener("change", function () {
   if (this.checked) {
     checkbox2.checked = false;
+
+    let ultimoHijo = contenido.lastChild;
+    contenido.removeChild(ultimoHijo);
+
+    ultimoHijo = contenido.lastChild;
+    contenido.removeChild(ultimoHijo);
+
+
+    /*verifica si la cantidad de hijos del elemento con ID "carritoFin" 
+    ha aumentado desde un punto anterior. Si la cantidad de hijos ha aumentado, se selecciona y
+    elimina el último hijo del elemento. Que aqui estaria eliminando el hijo que muestra los datos del cliente
+    con los atributos de los input: quien retira por la sucursal principal*/
+    let hijosCarritoDespues = carritoFin.children.length;
+    if(hijosCarritoDespues > hijosCarritoAntes)
+    {
+      ultimoHijo = carritoFin.lastChild;
+      carritoFin.removeChild(ultimoHijo);
+    }
+    
+
 
     //cambio el costo de envio de 0.0 a -> "$ Esperando codigo postal..." ya que todavia no se coloco el codigo postal para calcular el envio
     let envio = document.querySelector("#costo-envio");
     envio.innerHTML = `
     <p>${"$ Esperando codigo postal..."}</p>
      `;
+    let ciudad = document.createElement("input");
+    ciudad.classList.add("direccionCodeCity");
+    ciudad.type = "text";
+    ciudad.placeholder = "Ingrese su ciudad...";
 
     //crear la caja de texto, para que el cliente coloque su ciudad
     let direccionInput = document.createElement("input");
     direccionInput.classList.add("direccionCodeCity");
     direccionInput.type = "text";
     direccionInput.placeholder = "Ingrese su dirección...";
+
+    //crear la caja de texto, para que el cliente coloque quien recibe el producto
+    let nombreQuienRecibe = document.createElement("input");
+    nombreQuienRecibe.classList.add("direccionCodeCity");
+    nombreQuienRecibe.type = "text";
+    nombreQuienRecibe.placeholder = "Ingrese nombre de quien recibe el paquete...";
 
     //crear el conteiner de codigoPostal, para que el cliente coloque su codigo postal(input) y si no lo sabe pueda consultarlo en el link(a) de correo argentino.
     let codigoPostalContainer = document.createElement("div");
@@ -125,6 +152,7 @@ checkbox1.addEventListener("change", function () {
     let calcularCostoEnvio = document.createElement("button");
     calcularCostoEnvio.textContent = "Calcular costo de Envío";
 
+
     /*si al apretar el boton de "calcular costo de envio" y si el input de codigo postal,ciudad y direccion tienen contenido, se modifica el 
     costo de envio a $1500 en la seccion del carrito y en la checkbox1 se cambia "$ Esperando codigo postal" a -> $1500" Y ademas se muestran los datos del cliente
     en la seccion carritoFin */
@@ -134,15 +162,18 @@ checkbox1.addEventListener("change", function () {
       let valorCPIngresado = codigoPostalInput.value;
       let ciudadIngresada = ciudad.value;
       let direccionIngresada = direccionInput.value;
+      let nombreRecibeIngresada = nombreQuienRecibe.value;
 
-      if (valorCPIngresado != "" && ciudadIngresada != "" && direccionIngresada != ""){
+      if (valorCPIngresado != "" && ciudadIngresada != "" && direccionIngresada != "") {
         let datosCliente = document.createElement("div");
+        datosCliente.id = "datosCliente00";
         datosCliente.style.marginLeft = "20px";
         datosCliente.innerHTML = `
-            <p>${"------------[ DATOS DEL CLIENTE ]------------"}</p>
-            <p>${"CIUDAD: " + ciudad.value}</p>
-            <p>${"DIRECCION: " + direccionInput.value}</p>
-            <p>${"CODIGO POSTAL: " + codigoPostalInput.value}</p>
+            <h3>${"------------[ ENVIO A DOMICILIO ]------------"}</h3>
+            <p>${"CIUDAD: " + ciudadIngresada}</p>
+            <p>${"DIRECCION: " + direccionIngresada}</p>
+            <p>${"QUIEN RECIBE: " + nombreRecibeIngresada}</p>
+            <p>${"CODIGO POSTAL: " + valorCPIngresado}</p>
           `;
         carritoFin.appendChild(datosCliente);
 
@@ -173,14 +204,49 @@ checkbox1.addEventListener("change", function () {
     });
 
     // se agregan los hijos a la seccion contenido, en orden: direccion->codigoPostal->boton:CalcularCostoEnvio
+    contenido.appendChild(ciudad);
     contenido.appendChild(direccionInput);
+    contenido.appendChild(nombreQuienRecibe);
     contenido.appendChild(codigoPostalContainer);
     contenido.appendChild(calcularCostoEnvio);
-    
+
   } else {
     checkbox2.checked = true;
   }
 });
+
+
+if (checkbox2.checked == true) {
+  let quienRetira = document.createElement("input");
+  quienRetira.classList.add("direccionCodeCity");
+  quienRetira.type = "text";
+  quienRetira.placeholder = "Nombre y apellido de quien retira el paquete...";
+  contenido.appendChild(quienRetira);
+
+  let cargarNombreQuienRetira = document.createElement("button");
+  cargarNombreQuienRetira.textContent = "Aplicar";
+  contenido.appendChild(cargarNombreQuienRetira);
+  cargarNombreQuienRetira.addEventListener("click", function () {
+    var flag = 0;
+    let quienRetiraIngresa = quienRetira.value;
+    if (quienRetiraIngresa != "") {
+      let datosCliente = document.createElement("div");
+      datosCliente.id = "datosCliente00";
+      datosCliente.style.marginLeft = "20px";
+      datosCliente.innerHTML = `
+            <h3>${"------------[ RETIRO POR SUCURSAL PRINCIPAL ]------------"}</h3>
+            <p>${"QUIEN RETIRA: " + quienRetiraIngresa}</p>
+          `;
+      carritoFin.appendChild(datosCliente);
+      cargarNombreQuienRetira.style.backgroundColor = "red";
+      cargarNombreQuienRetira.style.color = "white";
+      setTimeout(function () { cargarNombreQuienRetira.style.backgroundColor = "rgb(190, 190, 190)"; }, 2000);
+      cargarNombreQuienRetira.textContent = "Ya aplicado"; //cambia el texto del boton de "Calcular costo de Envío" -> "Ya calculado"
+      cargarNombreQuienRetira.disabled = true;
+    }
+  })
+
+}
 
 // que sucede a marcar la casilla "checkbox2 -> Retiro en puntos cercanos o sucursal"
 checkbox2.addEventListener("change", function () {
@@ -194,16 +260,63 @@ checkbox2.addEventListener("change", function () {
     `;
 
     /*al inicar marcada la pagina en este checkbox, sucedia que si despues cambiabamos a el otro checkbox1, 
-    el cual crea 3 hijos al .contenido (direccion - codigoPostal - boton:Calcular Costo Envio) y luego queriamos 
-    pasar de nuevo al checkbox2 esos hijos iban a seguir estando entonces se utiliza removechild, por 3 veces para 
-    eliminar esos 3 hijos que corresoponde al checkbox1 y no al 2. Dejando asi el unico hijo de contenido que 
-    comparten ambos checkbox (1y2) que es el input de ingresar ciudad*/
+    el cual crea 5 hijos al .contenido (ciudad - direccion - quien recibe - codigoPostal - boton:Calcular Costo Envio) y luego queriamos 
+    pasar de nuevo al checkbox2 esos hijos iban a seguir estando entonces se utiliza removechild, por 5 veces para 
+    eliminar esos 5 hijos que corresoponde al checkbox1 y no al 2.*/
+
     let ultimoHijo = contenido.lastChild;
     contenido.removeChild(ultimoHijo);
     ultimoHijo = contenido.lastChild;
     contenido.removeChild(ultimoHijo);
     ultimoHijo = contenido.lastChild;
     contenido.removeChild(ultimoHijo);
+    ultimoHijo = contenido.lastChild;
+    contenido.removeChild(ultimoHijo);
+    ultimoHijo = contenido.lastChild;
+    contenido.removeChild(ultimoHijo);
+
+
+    /*verifica si la cantidad de hijos del elemento con ID "carritoFin" 
+    ha aumentado desde un punto anterior. Si la cantidad de hijos ha aumentado, se selecciona y
+    elimina el último hijo del elemento. Que aqui estaria eliminando el hijo que muestra los datos del cliente
+    con los atributos de los input: ciudad, direccion, quien recibe, codigo postal, boton de calcular costo de envio*/
+    let hijosCarritoDespues = carritoFin.children.length;
+    if(hijosCarritoDespues > hijosCarritoAntes)
+    {
+      ultimoHijo = carritoFin.lastChild;
+      carritoFin.removeChild(ultimoHijo);
+    }
+
+    let quienRetira = document.createElement("input");
+    quienRetira.classList.add("direccionCodeCity");
+    quienRetira.type = "text";
+    quienRetira.placeholder = "Nombre y apellido de quien retira el paquete...";
+    contenido.appendChild(quienRetira);
+
+    let cargarNombreQuienRetira = document.createElement("button");
+    cargarNombreQuienRetira.textContent = "Aplicar";
+    contenido.appendChild(cargarNombreQuienRetira);
+    cargarNombreQuienRetira.addEventListener("click", function () {
+      let quienRetiraIngresa = quienRetira.value;
+      if (quienRetiraIngresa != "") {
+        let datosCliente = document.createElement("div");
+        datosCliente.id = "datosCliente00";
+        datosCliente.style.marginLeft = "20px";
+        datosCliente.innerHTML = `
+            <h3>${"------------[ RETIRO POR SUCURSAL PRINICPAL ]------------"}</h3>
+            <p>${"QUIEN RETIRA: " + quienRetiraIngresa}</p>
+          `;
+        carritoFin.appendChild(datosCliente);
+        cargarNombreQuienRetira.disabled = true;
+      }
+      cargarNombreQuienRetira.style.backgroundColor = "red";
+      cargarNombreQuienRetira.style.color = "white";
+      setTimeout(function () { cargarNombreQuienRetira.style.backgroundColor = "rgb(190, 190, 190)"; }, 2000);
+      cargarNombreQuienRetira.textContent = "Ya aplicado"; //cambia el texto del boton de "Calcular costo de Envío" -> "Ya calculado"
+      cargarNombreQuienRetira.disabled = true;
+    })
+
+
 
 
     /*si se marca el primer checkbox y se coloca el codigo postal pero despues se arrepiente y 
@@ -224,6 +337,17 @@ checkbox2.addEventListener("change", function () {
     checkbox1.checked = true;
   }
 });
+
+
+const botonMercadoPago = document.querySelector("#mp-btn");
+botonMercadoPago.addEventListener("click", function () {
+  const datosCliente = document.querySelector("#datosCliente00");
+  const informacion = datosCliente.innerHTML;
+  alert(informacion);
+
+  // LA IDEA ACA ES MANDAR AL MAIL DE LA EMPRESA, LA ORDEN DE COMPRA Y UTLIZAR LA API DE MERCADOPAGO PARA PODER REALIZAR EL PAGO
+});
+
 
 
 
