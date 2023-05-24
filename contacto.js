1eb246e8ce6a5c0cec17586b4b8002d7f0fff540
yuske <yuske2108@gmail.com>
2023-05-24 20:15:19 -0300

    Sacamos la api the simpsons

--- contacto.js
+++ contacto.js
@@ -89,47 +89,47 @@ const input = document.querySelector('input');
 
     marvel.render();
 
-    // Función para obtener personajes aleatorios
-    function getRandomCharacters() {
-      fetch('https://apisimpsons.fly.dev/api/personajes')
-        .then((response) => response.json())
-        .then((data) => {
+// Función para obtener personajes aleatorios
+function getRandomCharacters() {
+  fetch('https://apisimpsons.fly.dev/api/personajes')
+    .then((response) => response.json())
+    .then((data) => {
           // Obtener 5 personajes aleatorios
-          const randomCharacters = getRandomItems(data, 5);
+    const randomCharacters = getRandomItems(data, 5);
 
           // Obtener el contenedor donde se mostrarán los personajes
-          const charactersContainer = document.getElementById('characters-container');
+    const charactersContainer = document.getElementById('characters-container');
 
           // Limpiar el contenedor
-          charactersContainer.innerHTML = '';
+    charactersContainer.innerHTML = '';
 
           // Mostrar los personajes
-          randomCharacters.forEach((character) => {
-            // Crear un elemento de imagen y establecer la URL de la imagen
-            const image = document.createElement('img');
-            image.src = character.imagen;
+    randomCharacters.forEach((character) => {
+          // Crear un elemento de imagen y establecer la URL de la imagen
+    const image = document.createElement('img');
+    image.src = character.imagen;
 
-            // Crear un elemento de párrafo y establecer el nombre del personaje
-            const name = document.createElement('p');
-            name.textContent = character.nombre;
+          // Crear un elemento de párrafo y establecer el nombre del personaje
+    const name = document.createElement('p');
+    name.textContent = character.nombre;
 
-            // Crear un contenedor para el personaje y añadir la imagen y el nombre
-            const characterContainer = document.createElement('div');
-            characterContainer.appendChild(image);
-            characterContainer.appendChild(name);
+          // Crear un contenedor para el personaje y añadir la imagen y el nombre
+    const characterContainer = document.createElement('div');
+    characterContainer.appendChild(image);
+    characterContainer.appendChild(name);
 
-            // Añadir el contenedor del personaje al contenedor principal
-            charactersContainer.appendChild(characterContainer);
-          });
-        })
-        .catch((error) => {
-          console.log('Error:', error);
-        });
+          // Añadir el contenedor del personaje al contenedor principal
+    charactersContainer.appendChild(characterContainer);
+      });
+    })
+    .catch((error) => {
+      console.log('Error:', error);
+      });
     }
 
     // Función para obtener elementos aleatorios de un arreglo
     function getRandomItems(array, count) {
-      const shuffled = array.sort(() => 0.5 - Math.random());
+      const shuffled = array.sort(() =>  - Math.random());
       return shuffled.slice(0, count);
     }
 
